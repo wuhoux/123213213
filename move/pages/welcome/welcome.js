e36c5a092219cbc99f4718204aa85eca604d1c66
那一抹阳光 <xpf815824935@users.noreply.github.com>
2017-07-07 21:48:32 +0800

    Add files via upload

---
+++ move/pages/welcome/welcome.js
@@ -0,0 +1,90 @@
+// pages/welcome/welcome.js
+var app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    inTheaters:{}   
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var inTheatersURL = app.globalData.doubanBase + app.globalData.inTheaters + "?start=0&count=10";
+    console.log(inTheatersURL);
+    this.getMovieListData(inTheatersURL,"inTheatersURL","影院热映");
+   // this.getMovieListData(comingSoonURL,"comingSoonURL","即将上映");
+  },
+  getMovieListData: function(url,settedKey,categoryTitle)
+   {
+     wx.showToast({
+       title:'加载中',
+       icon: 'loading',
+       duration: 10000
+     });
+     var that =this;
+     wx.request ({
+       url: url,
+       method:'GET',
+       header:{
+         "content-type":"json"
+       },
+        success: function(res) {
+        var data = res.data;
+        console.log(data);
+        }
+     })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
